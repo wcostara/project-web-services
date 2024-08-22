ff4574eeb146fa77a5452348de60900fe1111fd4
Will Costa <wcostarabr@hotmail.com>
2024-08-22 12:37:58 -0300

    project webServices

---
+++ src/main/java/com/costara/project_dikaion/ProjectDikaionApplication.java
@@ -0,0 +1,13 @@
+package com.costara.project_dikaion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDikaionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDikaionApplication.class, args); //run spring boot4
+	}
+
+}
---
+++ src/test/java/com/costara/project_dikaion/ProjectDikaionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costara.project_dikaion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDikaionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
