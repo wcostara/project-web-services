e3ab915218a79e054d0d5c30001d3f3231dbc513
Will Costa <wcostarabr@hotmail.com>
2024-08-23 13:46:43 -0300

    OrderStatus enum

--- src/main/java/com/costara/project_dikaion/config/TestConfig.java
+++ src/main/java/com/costara/project_dikaion/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.costara.project_dikaion.entities.Order;
 import com.costara.project_dikaion.entities.User;
+import com.costara.project_dikaion.entities.enums.OrderStatus;
 import com.costara.project_dikaion.repositories.OrderRepository;
 import com.costara.project_dikaion.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Bethy White", "bethy@gmail.com", "8888-8888", "54321");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "7777-7777", "98745");
 		
-		Order o1 = new Order(null, Instant.parse("2024-03-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2024-06-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2024-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2024-03-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2024-06-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2024-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
