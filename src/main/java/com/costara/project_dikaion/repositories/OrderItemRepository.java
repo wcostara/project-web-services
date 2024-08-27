07ed9327ff4eeb1ebb10f6f53db68cd1d6cf74ed
Will Costa <wcostarabr@hotmail.com>
2024-08-27 13:18:52 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/costara/project_dikaion/repositories/OrderItemRepository.java
+++ src/main/java/com/costara/project_dikaion/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.costara.project_dikaion.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.costara.project_dikaion.entities.OrderItem;
+import com.costara.project_dikaion.entities.pk.OrderItemPk;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPk>{
 
 }
