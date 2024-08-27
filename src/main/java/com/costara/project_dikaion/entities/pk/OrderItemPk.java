63fd6f6c7f3f2f62906952ace92e7f80941ea0c9
Will Costa <wcostarabr@hotmail.com>
2024-08-27 17:40:46 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/costara/project_dikaion/entities/pk/OrderItemPk.java
+++ src/main/java/com/costara/project_dikaion/entities/pk/OrderItemPk.java
@@ -12,7 +12,8 @@ import jakarta.persistence.ManyToOne;
 
 @Embeddable
 public class OrderItemPk implements Serializable {
-	
+	private static final long serialVersionUID = 1L;
+
 	@ManyToOne
 	@JoinColumn(name = "order_id")
 	private Order order;
