e53af095d4207df5ce8928b12d1cf0692fedabb4
Will Costa <wcostarabr@hotmail.com>
2024-08-30 13:56:20 -0300

    User insert

--- src/main/java/com/costara/project_dikaion/resources/UserResource.java
+++ src/main/java/com/costara/project_dikaion/resources/UserResource.java
@@ -1,16 +1,22 @@
 package com.costara.project_dikaion.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.costara.project_dikaion.entities.User;
 import com.costara.project_dikaion.services.UserService;
+
+import jakarta.websocket.server.ServerApplicationConfig;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -28,6 +34,13 @@ public class UserResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.ok().body(obj);
 	}
 	
