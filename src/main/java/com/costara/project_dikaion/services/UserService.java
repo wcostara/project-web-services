c5df3f39d8b701408e5bf1c58a478cbf2884b324
Will Costa <wcostarabr@hotmail.com>
2024-08-30 16:25:37 -0300

    Exception handling - findById

--- src/main/java/com/costara/project_dikaion/services/UserService.java
+++ src/main/java/com/costara/project_dikaion/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.costara.project_dikaion.entities.User;
 import com.costara.project_dikaion.repositories.UserRepository;
+import com.costara.project_dikaion.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById (Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
