df3ef4d796ecc776d5e1639bef69602e1bfc9eb7
Will Costa <wcostarabr@hotmail.com>
2024-08-30 17:25:18 -0300

    Exception handling - delete

--- src/main/java/com/costara/project_dikaion/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/costara/project_dikaion/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.costara.project_dikaion.services.exceptions.DatabaseException;
 import com.costara.project_dikaion.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> ResourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource Not Found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> dataBase(DatabaseException e, HttpServletRequest request){
+		String error = "DataBase Error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
--- src/main/java/com/costara/project_dikaion/services/UserService.java
+++ src/main/java/com/costara/project_dikaion/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.costara.project_dikaion.entities.User;
 import com.costara.project_dikaion.repositories.UserRepository;
+import com.costara.project_dikaion.services.exceptions.DatabaseException;
 import com.costara.project_dikaion.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,6 +34,13 @@ public class UserService {
 	
 	public void delete(Long id) {
 		repository.deleteById(id);
+		try{
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
